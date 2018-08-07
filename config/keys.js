5c290d5cd12434f65e4a7c619d40aea4ebd3b60e
Wout Heijnen <woutheijnen@hotmail.com>
2018-08-08 01:31:09 +0200

    Creating The JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://wout:wout123@ds213472.mlab.com:13472/devconnector-q772cm'
+  mongoURI: 'mongodb://wout:wout123@ds213472.mlab.com:13472/devconnector-q772cm',
+  secretOrKey: 'secret'
 }
